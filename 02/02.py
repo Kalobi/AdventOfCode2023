bd6ec8a246c7fb958202b6e645da9142088e0a55
Kalobi <46748261+Kalobi@users.noreply.github.com>
2023-12-14 20:13:58 +0100

    Day 2 Part 2

--- 02/02.py
+++ 02/02.py
@@ -18,8 +18,30 @@ def is_game_valid(game: str, bag: dict):
     return all(is_subgame_valid(subgame, bag) for subgame in parse_game(game))
 
 
+def valid_games_total_id(games):
+    return sum(int(game.partition(": ")[0].removeprefix("Game ")) for game in games if is_game_valid(game, bag))
+
+
+def minimum_bag(game: str):
+    parsed = parse_game(game)
+    bag = {}
+    for subgame in parsed:
+        for color, count in subgame.items():
+            if color not in bag or bag[color] < count:
+                bag[color] = count
+    return bag
+
+
+def power(bag: dict):
+    return bag.get("red", 0) * bag.get("green", 0) * bag.get("blue", 0)
+
+
+def total_minimum_power(games):
+    return sum(power(minimum_bag(game)) for game in games)
+
+
 if __name__ == '__main__':
     total = 0
     bag = {"red": 12, "green": 13, "blue": 14}
     with open("input.txt") as f:
-        print(sum(int(line.partition(": ")[0].removeprefix("Game ")) for line in f if is_game_valid(line, bag)))
+        print(total_minimum_power(f))
